e8df241529d4b23fd4d25ee72f2126884681ea79
Bilal Asif Mirza <bilal.asif.97@gmail.com>
2025-12-30 23:42:09 +0500

    Checking vars in deployment

--- src/main/kotlin/purl/url/PurlApplication.kt
+++ src/main/kotlin/purl/url/PurlApplication.kt
@@ -7,5 +7,8 @@ import org.springframework.boot.runApplication
 class PurlApplication
 
 fun main(args: Array<String>) {
+    System.getenv().forEach { (key, value) ->
+        println("$key=$value")
+    }
     runApplication<PurlApplication>(*args)
 }
